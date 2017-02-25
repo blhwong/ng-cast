7ce75839ba2694b51896621737002e84789f2921
Hack Reactor Students <accounts@hackreactor.com>
2017-02-25 13:49:40 -0800

    Renders videoListEntry onClick

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,15 @@
 angular.module('video-player')
-// .controller('appController', function($scope) {
-//   $scope.videoList = window.exampleVideoData;
-//   $scope.currentVideo = window.exampleVideoData[0];
+//  .controller('appController', function($scope) {
+// //   $scope.videoList = window.exampleVideoData;
+// //   $scope.currentVideo = window.exampleVideoData[0];
 
 
 //   $scope.selectVideo = function(obj) {
+//     debugger;
 //     console.log('click');
 //     $scope.currentVideo = obj;
 //   };
-// })
+//  })
 .directive('app', function() {
   return { 
     scope: {
@@ -20,11 +21,16 @@ angular.module('video-player')
     controllerAs: 'ctrl',
     bindToController: true,
     controller: function($scope) {
+      this.currentVideo = window.exampleVideoData[0];
+      this.videos = window.exampleVideoData;
+      this.selectVideo = function(obj) {
+        // console.log('click');
+        // debugger;
+        this.currentVideo = obj;
+        // console.log(this.currentVideo);
+      }.bind(this);
+      this.searchResults = function() {};
       // debugger;
-      // this.selectVideo = function() {};
-      // ctrl.videoList = window.exampleVideoData;
-      // $scope.currentVideo = window.exampleVideoData[0];
-
 
       // this.selectVideo = function(obj) {
       //   console.log('click');
--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -4,7 +4,7 @@ angular.module('video-player')
   return {
     // TODO
     scope: {
-      video: '<'
+      video: '='
     },
     controllerAs: 'ctrl',
     bindToController: true,
